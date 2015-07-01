d9e98b9c88e001cffbc9ed1b7db2782346aa0bd7
Rob Pongsajapan <rap42@georgetown.edu>
2015-07-01 15:23:25 -0400

    fix script

--- export-text.py
+++ export-text.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('corpus.csv', 'rU') as f:
+with open('data/corpus.csv', 'rU') as f:
   reader = csv.reader(f, delimiter=",")
   for row in reader:
     f = open('data/output/' + str(row[0]) + '.txt', 'w')
