44935cf0e4f7af93ed7783703aa52e18cef51ab2
Shannon Mooney <smm223@gmail.com>
2015-07-28 11:31:46 -0400

    switched wrappers to one that works

--- stanforddep.py
+++ stanforddep.py
@@ -1,4 +1,4 @@
-from stanford.corenlp import *
+from stanford_corenlp_pywrapper import CoreNLP
 from nltk import *
 
 with open("data/engelhardtest.txt", 'rU') as f:
@@ -6,5 +6,7 @@ with open("data/engelhardtest.txt", 'rU') as f:
 	engelhard2 = engelhard.decode('utf8')
 #print(engelhard)
 
-corenlp = StanfordCoreNLP(corenlp_path = "./stanford/stanford-corenlp-full-2014-08-27/")
-corenlp.parse(engelhard2)
+proc = CoreNLP("parse", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
+trees = proc.parse_doc(engelhard2)
+
+print(trees)
