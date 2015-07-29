fb6e795061b57c5ff48afa65fd46635eefef4ced
Shannon Mooney <smm223@gmail.com>
2015-07-29 16:53:27 -0400

    updated to process directory of text files

--- stanforddep.py
+++ stanforddep.py
@@ -1,12 +1,55 @@
+from __future__ import print_function
+
 from stanford_corenlp_pywrapper import CoreNLP
 from nltk import *
+import os
 
-with open("data/engelhardtest.txt", 'rU') as f:
-	engelhard = f.read()
-	engelhard2 = engelhard.decode('utf8')
+#correct subdirectory by coded type goes here
+#comment all this to do a single text file instead of a directory
+path = 'data/engelhard/0/'
+for filename in os.listdir(path):
+	with open(path+filename, 'rU') as f:
+		engelhard = f.read()
+		engelhard2 = engelhard.decode('utf8', 'ignore')
+	#	print(engelhard2)
+
+	#this is set as parse (parsing with named entity recognition) but you can also change it to different options, like:
+	#ssplit for tokenization and sentence splitting
+	#pos for pos and lemmas
+	#ner for pos and ner and lemmas
+	#parse for pos, lemmas, trees, dependencies
+	#nerparse for parsing with ner, pos, lemmas, dependencies
+	#coref for coreference including constituent parsing
+
+	#comment this to do coref
+	proc = CoreNLP("parse", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
+	trees = proc.parse_doc(engelhard2)
+	print(trees)
+
+	#uncomment this to do coref
+	# proc = CoreNLP("coref", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
+	# coref = proc.parse_doc(engelhard2)
+	# print(coref)
+
+#uncomment this to do a single text file instead of a directory
+#with open("data/engelhardtest.txt", 'rU') as f:
+#	engelhard = f.read()
+#	engelhard2 = engelhard.decode('utf8')
 #print(engelhard)
+#this is set as parse (parsing with named entity recognition) but you can also change it to different options, like:
+#ssplit for tokenization and sentence splitting
+#pos for pos and lemmas
+#ner for pos and ner and lemmas
+#parse for pos, lemmas, trees, dependencies
+#nerparse for parsing with ner, pos, lemmas, dependencies
+#coref for coreference including constituent parsing
+#comment this to do coref
+# proc = CoreNLP("parse", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
+# trees = proc.parse_doc(engelhard2)
+# print(trees)
+#uncomment this to do coref
+# proc = CoreNLP("coref", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
+# coref = proc.parse_doc(engelhard2)
+# print(coref)
 
-proc = CoreNLP("parse", corenlp_jars=["stanford/stanford-corenlp-full-2014-08-27/*"])
-trees = proc.parse_doc(engelhard2)
-
-print(trees)
+#make print statement as argument in command!
