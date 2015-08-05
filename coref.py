689a76f8a3c99587447d811ea0fb6e11af2501cd
Rob Pongsajapan <rap42@georgetown.edu>
2015-08-05 15:04:35 -0400

    fix stupid coref bug

--- coref.py
+++ coref.py
@@ -1,15 +1,14 @@
 from stanford_corenlp_pywrapper import CoreNLP
 from pprint import pprint
-import os
+import glob
 
 proc = CoreNLP("coref", corenlp_jars=["stanford/stanford-corenlp-full-2015-04-20/*"])
 path = 'data/engelhard/0/'
-for filename in os.listdir(path):
+for filename in glob.glob(path+'*.txt'):
   print(filename)
-  with open(path+filename, 'rU') as f:
+  with open(filename, 'rU') as f:
     engelhard = f.read()
     engelhard2 = engelhard.decode('utf8', 'ignore')
     print(engelhard2)
     a = proc.parse_doc(engelhard2)['entities']
   pprint(a)
-
